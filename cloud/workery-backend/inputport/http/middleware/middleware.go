bf23cf9436a0ae8967be43b7976046680318f5fe
Bartlomiej Mika <bart@mikasoftware.com>
2024-08-08 17:13:18 -0400

    Made the `EnforceBlacklistMiddleware` middleware less wordy in console log.

--- cloud/workery-backend/inputport/http/middleware/middleware.go
+++ cloud/workery-backend/inputport/http/middleware/middleware.go
@@ -86,27 +86,44 @@ func (mid *middleware) EnforceBlacklistMiddleware(next http.HandlerFunc) http.Ha
 
 		// Case 1 of 2: Check banned IP addresses.
 		if mid.Blacklist.IsBannedIPAddress(ipAddress) {
-			mid.Logger.Warn("rejected request by ip",
-				slog.Any("url", r.URL.Path),
-				slog.String("ip_address", ipAddress),
-				slog.String("proxies", proxies),
-				slog.Any("middleware", "EnforceBlacklistMiddleware"))
+
+			// If the client IP address is banned, check to see if the client
+			// is making a call to a URL which is not banned, and if the URL
+			// is not banned (has not been banned before) then print it to
+			// the console logs for future analysis. Else if the URL is banned
+			// then don't bother printing to console. The purpose of this code
+			// is to not clog the console log with warnings.
+			if !mid.Blacklist.IsBannedURL(r.URL.Path) {
+				mid.Logger.Warn("rejected request by ip",
+					slog.Any("url", r.URL.Path),
+					slog.String("ip_address", ipAddress),
+					slog.String("proxies", proxies),
+					slog.Any("middleware", "EnforceBlacklistMiddleware"))
+			}
 			http.Error(w, "forbidden at this time", http.StatusForbidden)
 			return
 		}
 
 		// Case 2 of 2: Check banned URL.
 		if mid.Blacklist.IsBannedURL(r.URL.Path) {
-			mid.Logger.Warn("rejected request by url",
-				slog.Any("url", r.URL.Path),
-				slog.String("ip_address", ipAddress),
-				slog.String("proxies", proxies),
-				slog.Any("middleware", "EnforceBlacklistMiddleware"))
+
+			// If the URL is banned, check to see if the client IP address is
+			// banned, and if the client has not been banned before then print
+			// to console the new offending client ip. Else do not print if
+			// the offending client IP address has been banned before. The
+			// purpose of this code is to not clog the console log with warnings.
+			if !mid.Blacklist.IsBannedIPAddress(ipAddress) {
+				mid.Logger.Warn("rejected request by url",
+					slog.Any("url", r.URL.Path),
+					slog.String("ip_address", ipAddress),
+					slog.String("proxies", proxies),
+					slog.Any("middleware", "EnforceBlacklistMiddleware"))
+			}
 
 			// DEVELOPERS NOTE:
 			// Simply return a 404, but in our console log we can see the IP
 			// address whom made this call.
-			http.Error(w, "does not exist", http.StatusNotFound)
+			http.Error(w, "does not exist at this time", http.StatusNotFound)
 			return
 		}
 
