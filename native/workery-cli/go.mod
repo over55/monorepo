e980862ec73a79f234671d68fd2890a5521f8a92
Bartlomiej Mika <bart@mikasoftware.com>
2025-08-12 23:19:30 -0400

    Deprecated the current CLI and starting new CLI. Related to issue #1 via https://github.com/over55/monorepo/issues/1.

---
+++ native/workery-cli/go.mod
@@ -0,0 +1,3 @@
+module github.com/over55/monorepo/cloud/workery-cli
+
+go 1.24.4
---
+++ native/workery-cli/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/over55/monorepo/cloud/workery-cli/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
