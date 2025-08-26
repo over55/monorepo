81ce60739a9068b96c0d281f886e1c5ca239de42
Bartlomiej Mika <bart@mikasoftware.com>
2025-08-26 18:34:06 -0400

    Refactor: Remove file path comment
    https://github.com/over55/monorepo/issues/67

--- web/workery-frontend-deprecated/src/Components/Admin/Help.jsx
+++ web/workery-frontend-deprecated/src/Components/Admin/Help.jsx
@@ -1,3 +1,4 @@
+// File Path: monorepo/web/workery-frontend-deprecated/src/Components/Admin/Help.jsx
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Scroll from "react-scroll";
